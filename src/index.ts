146bf9f384ae2d34ce7cb6b7bb047eee16e9bfe3
Liam Schuitemaker <liamschuitemaker@StephenHP.localdomain>
2025-04-29 18:40:47 -0700

    Add prisma db query

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,18 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const connectionString =
+			'postgresql://prisma-test_owner:npg_K53dBEAXxCes@ep-flat-mouse-a4t436m4-pooler.us-east-1.aws.neon.tech/prisma-test?sslmode=require';
+
+		const adapter = new PrismaPg({ connectionString });
+		const prisma = new PrismaClient({ adapter });
+
+		const posts = await prisma.post.findMany();
+
+		return Response.json(posts);
 	},
 } satisfies ExportedHandler<Env>;
